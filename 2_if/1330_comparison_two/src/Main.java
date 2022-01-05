eb404a2968730c9e90909a5236c567aa6d158136
Junhyung-Oh <chess109@naver.com>
2022-01-05 20:54:06 +0900

    commit 1330

--- 2_if/1330_comparison_two/src/Main.java
+++ 2_if/1330_comparison_two/src/Main.java
@@ -1,2 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = 0 , b = 0;
+        try{
+            a = sc.nextInt();
+            b = sc.nextInt();
+            sc.close();
+        }
+        catch (InputMismatchException e){
+            e.printStackTrace();
+        }
+        if(a > b) System.out.println(">");
+        else if(a == b) System.out.println("==");
+        else System.out.println("<");
+    }
 }
